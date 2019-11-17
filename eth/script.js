5320f694829b2992d40b04b01ec41fe3c2045bd6
Mlodat <57733503+Mlodat@users.noreply.github.com>
2019-11-17 13:01:16 +0300

    Update script.js

--- eth/script.js
+++ eth/script.js
@@ -10,15 +10,21 @@ request.onreadystatechange = (e) => {
 	
 	//let param1 = params[0];
 	let param1 = a;
-	let param2 = params[1];
-	let param3 = params[2];
+	let param2 = a;
+	let param3 = a;
+	let param4 = a;
+	//let param2 = params[1];
+	//let param3 = params[2];
+	//let param4 = params[3];
 
 	elemParam1 = document.getElementById('param1');
-	elemParam1.innerHTML = 16;
+	elemParam1.innerHTML = param1;
 	elemParam2 = document.getElementById('param2');
 	elemParam2.innerHTML = param2;
 	elemParam3 = document.getElementById('param3');
 	elemParam3.innerHTML = param3;
+	elemParam4 = document.getElementById('param4');
+	elemParam4.innerHTML = param4;
 }
 
 function createRequest() {
